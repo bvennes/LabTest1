eccc849c6ab93a44f4905a44de3ef08c9f15b0f0
Branden Vennes <vennes19@up.edu>
2016-08-30 21:56:24 -0700

    Initial Commit

---
+++ app/src/main/java/edu/up/cs371/vennes19/labtest1/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.up.cs371.vennes19.labtest1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
